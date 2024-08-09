5ff057ce03c00d4181ec93e7bad3c46f8022ec9b
abhay0031 <abhayjeet0031@gmail.com>
2024-08-09 11:53:15 +0530

    Add files via upload

---
+++ Day16/Solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        // first method
+        // int n=nums.length;
+        // int sum1=n*(n+1)/2;
+        // int sum=0;
+        // for(int i=0;i<nums.length;i++){
+
+        //     sum=sum+nums[i];
+        // }
+
+        // int MissingNum=sum1-sum;
+        // return MissingNum;
+
+        // second methood  bit manipulation
+
+        int n=nums.length;
+
+        for(int i=0;i<n;i++){
+
+            
+        }
+    }
+}
