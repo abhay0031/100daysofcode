fa5f1c74c46ff163ef9dce56601da073b64a9f13
abhay0031 <abhayjeet0031@gmail.com>
2024-09-17 11:43:52 +0530

    Add files via upload

---
+++ Day56/Reverse_Words_in_a_String.java
@@ -0,0 +1,13 @@
+class Reverse_Words_in_a_String{
+    public String reverseWords(String s) {
+        String[] words = s.trim().split("\\s+");
+        
+        StringBuilder reversed = new StringBuilder();
+
+        for (int i = words.length - 1; i >= 0; i--) {
+            reversed.append(words[i]).append(" ");
+        }
+        
+        return reversed.toString().trim();
+    }
+}
